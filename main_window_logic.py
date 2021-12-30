572413fe622fac3caf67cbc204c2e37ea124d970
artem <notelsecode@gmail.com>
2021-12-30 22:01:33 +0400

    1) переименован объект финиша теста 2) слоты созданы, сигналы к ним привязаны, осталось добавить содержание к слотам

--- main_window_logic.py
+++ main_window_logic.py
@@ -4,16 +4,31 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
 
 from main_window_design import Ui_MainWindow
 
+PREVIOUS = "previous"  # ключ предыдущего вопроса
+NEXT = "next"  # ключ следующего вопроса
+
 
 class MyMainWindow(QMainWindow, Ui_MainWindow):
 
     def __init__(self):
         super(MyMainWindow, self).__init__()
         self.setupUi(self)
+        self.result = 0  # текущий результат
+        self.count = 0  # текущее количество заданий, определяется после парсинга файла
 
+        # соединение сигналов со слотами
         self.open_test_action.triggered.connect(self.open_file)
         self.statusbar.messageChanged.connect(lambda args: self.status_changed(args))
-        # TODO: дописать слоты и связать кнопки с ними
+        self.finish_test_button.clicked.connect(self.finish_test)
+        self.previous_question_button.clicked.connect(lambda: self.show_question(key=PREVIOUS))
+        self.next_question_button.clicked.connect(lambda: self.show_question(key=NEXT))
+
+    def show_question(self, key):
+        # логгирование
+        print("clicked", self.sender().text(), f"with key {key}")
+
+    def finish_test(self):
+        self.question_label.setText(f"Тест окончен, ваш результат: {self.result} из {self.count}")
 
     def status_changed(self, args):
         if not args:
@@ -37,9 +52,9 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
 
     def enable(self):
         self.question_label.setEnabled(True)
-        self.previous_question.setEnabled(True)
-        self.next_question.setEnabled(True)
-        self.pushButton.setEnabled(True)  # TODO: изменить название объекта через дизайн
+        self.previous_question_button.setEnabled(True)
+        self.next_question_button.setEnabled(True)
+        self.finish_test_button.setEnabled(True)
         self.answer_line_edit.setEnabled(True)
 
     def parse_file(self, path):
