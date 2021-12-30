33c306545fe40bace538993300a621aef947d190
artem <notelsecode@gmail.com>
2021-12-31 00:19:18 +0400

    first stable version

--- main_window_logic.py
+++ main_window_logic.py
@@ -1,7 +1,6 @@
 import sys
-from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
-
+import pandas as pd
 from main_window_design import Ui_MainWindow
 
 PREVIOUS = "previous"  # ключ предыдущего вопроса
@@ -15,6 +14,9 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         self.result = 0  # текущий результат
         self.count = 0  # текущее количество заданий, определяется после парсинга файла
+        self.questions = list()
+        self.current_question = 0
+        self.answers = list()
 
         # соединение сигналов со слотами
         self.open_test_action.triggered.connect(self.open_file)
@@ -22,13 +24,54 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
         self.finish_test_button.clicked.connect(self.finish_test)
         self.previous_question_button.clicked.connect(lambda: self.show_question(key=PREVIOUS))
         self.next_question_button.clicked.connect(lambda: self.show_question(key=NEXT))
+        self.answer_line_edit.returnPressed.connect(self.check_answer)
 
-    def show_question(self, key):
-        # логгирование
-        print("clicked", self.sender().text(), f"with key {key}")
+    def check_answer(self):
+        if self.answer_line_edit.text() == self.questions[self.current_question][1]:
+            self.result += 1
+            self.result = min(self.result, self.count)
+        self.answers[self.current_question] = self.answer_line_edit.text()
+        if self.current_question != self.count - 1:
+            self.statusBar().showMessage("Ответ сохранён.", 5000)
+        elif all(self.answers):
+            self.statusBar().showMessage("Ответ сохранён. Вы ответили на все вопросы можете завершить тест.", 5000)
+        else:
+            self.statusBar().showMessage(
+                "Ответ сохранён. Вы ответили на последний вопрос, но у вас остались вопросы без ответа.", 5000)
+        self.statusBar().setStyleSheet("background-color: green")
+
+    def show_question(self, key=None):
+        if key == PREVIOUS and self.current_question != 0:
+            self.current_question -= 1
+        elif key == NEXT and self.current_question != self.count - 1:
+            self.check_answer()
+            self.current_question += 1
+        if self.current_question == 0:
+            self.previous_question_button.setEnabled(False)
+        else:
+            self.previous_question_button.setEnabled(True)
+        if self.current_question == self.count - 1:
+            self.next_question_button.setEnabled(False)
+        else:
+            self.next_question_button.setEnabled(True)
+        self.question_label.setText(self.questions[self.current_question][0])
+        self.answer_line_edit.setText(self.answers[self.current_question])
+        self.statusBar().setStyleSheet("background-color: #EFEFEF")
+        self.statusBar().showMessage("")
 
     def finish_test(self):
-        self.question_label.setText(f"Тест окончен, ваш результат: {self.result} из {self.count}")
+        self.check_answer()
+        sep = '\n'
+        no_answer = "Нет ответа"
+        self.question_label.setText(
+            f"Тест окончен, ваш результат: {self.result} из {self.count}\n"
+            f"Ваши ответы - правильные ответы:\n"
+            f"{sep.join(f'{self.answers[current] if self.answers[current] else no_answer} - {self.questions[current][1]}' for current in range(self.count))}")
+        self.previous_question_button.setEnabled(False)
+        self.next_question_button.setEnabled(False)
+        self.finish_test_button.setEnabled(False)
+        self.answer_line_edit.clear()
+        self.answer_line_edit.setEnabled(False)
 
     def status_changed(self, args):
         if not args:
@@ -51,6 +94,11 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
             self.statusBar().setStyleSheet("background-color: red")
 
     def enable(self):
+        self.result = 0
+        self.count = 0
+        self.questions = list()
+        self.current_question = 0
+        self.answers = list()
         self.question_label.setEnabled(True)
         self.previous_question_button.setEnabled(True)
         self.next_question_button.setEnabled(True)
@@ -58,8 +106,12 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
         self.answer_line_edit.setEnabled(True)
 
     def parse_file(self, path):
-        # TODO: добавить парсинг Excel файла
-        pass
+        xl = pd.ExcelFile(path)
+        df1 = xl.parse(xl.sheet_names[0])
+        self.questions = [list(row) for row in df1.values]
+        self.count = len(self.questions)
+        self.answers = ["" for _ in range(self.count)]
+        self.show_question()
 
 
 if __name__ == "__main__":
